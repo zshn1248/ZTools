236374a178a84f2d3e3e8d877225bb180e8237b1
zshn1248 <127026805+zshn1248@users.noreply.github.com>
2023-10-24 06:23:14 +0000

    Updated

--- ztools.py
+++ ztools.py
@@ -1,52 +1,53 @@
 import getpass
 
-
 password = "zeeshan123"
 username = "zshn"
+
 userIn = str(input("Enter username: "))
 if userIn == "exit":
     exit()
 while userIn != username:
-    print("user not found!")
+    print("User not found!")
     userIn = input("Enter username: ")
     if userIn == "exit":
-        
-        # time.wait(3)
         exit()
-        
+
 passIn = getpass.getpass("Enter password: ")
 if passIn == "exit":
     exit()
 while passIn != password:
     if passIn == "exit":
         exit()
-    print("incorrect password!")
+    print("Incorrect password!")
     passIn = getpass.getpass("Enter password: ")
-if userIn == username and passIn == passIn:
-    print(f"user {username} logged in")
-    print("""
+
+print(f"User {username} logged in")
+print("""
 [0] Exit
 [1] Generate random wordlist
-
 """)
-    zcmd = input(f"{username}@zcmd> ")
-    if zcmd == "0":
-        exit()
-    elif zcmd == "1":
-        print("importing random")
-        import random
-        x = 0
-        y = int(input("Number of words: "))
-        wordLength = int(input("Enter word length: "))
-        charmap = "asdfghjklqwertyuiopzxcvbnm"
-        filename = input("Enter filename: ")
-        if ".txt" not in filename:
-            filename = filename+".txt"
-            file = open(f"{filename}", "a+")
-            while x < y:
-                random_word = ''.join(random.choice(charmap) for _ in range(wordLength))
-                print(random_word)
-                file.write(f"{random_word}\n")
-                x += 1
-            file.close()
-        
+
+zcmd = input(f"{username}@zcmd> ")
+
+if zcmd == "0":
+    exit()
+elif zcmd == "1":
+    import random
+    x = 0
+    y = int(input("Number of words: "))
+    wordLength = int(input("Enter word length: "))
+    charmap = "asdfghjklqwertyuiopzxcvbnm"
+    filename = input("Enter filename: ")
+
+    if ".txt" not in filename:
+        filename = filename + ".txt"
+
+    file = open(filename, "a+")
+
+    while x < y:
+        random_word = ''.join(random.choice(charmap) for _ in range(wordLength))
+        print(random_word)
+        file.write(f"{random_word}\n")
+        x += 1
+
+    file.close()
